44180aff540212fca7569040e5cb90ec457742c8
Evan Smith <me@iamevan.me>
2023-07-27 14:42:26 +0100

    Add information about blue-green and canary deployments

--- site-content/astro.config.mjs
+++ site-content/astro.config.mjs
@@ -14,6 +14,7 @@ export default defineConfig({
       social: {
         github: 'https://github.com/thejokersthief/cicdbestpractises',
       },
+      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4, },
       sidebar: [
         {
           label: 'Continuous Integration',
